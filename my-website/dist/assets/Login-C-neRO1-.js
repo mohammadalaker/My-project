4b280ad6815055f84a111ae2d2f90ba9eb9e6dee
Mohammad Alaker <mohammadalaker@gmail.com>
2026-02-25 23:16:28 +0200

    build: UI update for tablet visibility

--- my-website/dist/assets/Login-C-neRO1-.js
+++ my-website/dist/assets/Login-C-neRO1-.js
@@ -1,4 +1,4 @@
-import{c as i,r as s,j as e,E as f,a as g}from"./index-rxkOeVsj.js";/**
+import{c as i,r as s,j as e,E as f,a as g}from"./index-DbolUiqO.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
